b609dbce693a75b2414f73a60a6c447b89ef6a87
minsikk <131092169+1224kang@users.noreply.github.com>
2025-04-14 02:35:50 +0900

    build:Spring Boot 초기 세팅

---
+++ src/main/java/jpabasic/pinnolbe/PinnolBeApplication.java
@@ -0,0 +1,13 @@
+package jpabasic.pinnolbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PinnolBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PinnolBeApplication.class, args);
+	}
+
+}
