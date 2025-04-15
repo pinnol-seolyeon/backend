20ce6193befcd0caecf1304dd0d6d0dac3dbdf25
minsikk <131092169+1224kang@users.noreply.github.com>
2025-04-16 00:04:33 +0900

    feat: mongoDB 설정

--- src/main/java/jpabasic/pinnolbe/PinnolBeApplication.java
+++ src/main/java/jpabasic/pinnolbe/PinnolBeApplication.java
@@ -2,8 +2,10 @@ package jpabasic.pinnolbe;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableMongoRepositories
 public class PinnolBeApplication {
 
 	public static void main(String[] args) {
