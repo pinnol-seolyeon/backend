63fa118ca2d04f2e13bf220b307b6327ee19e59c
minsikk <131092169+1224kang@users.noreply.github.com>
2025-05-20 13:55:02 +0900

    feat: s3에 이미지 업로드 POST
    - s3에 이미지 업로드API 개발
    
    Related to : #14

--- src/main/java/jpabasic/pinnolbe/controller/StudyController.java
+++ src/main/java/jpabasic/pinnolbe/controller/StudyController.java
@@ -1,5 +1,8 @@
 package jpabasic.pinnolbe.controller;
 
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.ObjectMetadata;
 import io.swagger.v3.oas.annotations.Operation;
 import jpabasic.pinnolbe.domain.User;
 import jpabasic.pinnolbe.domain.study.Chapter;
@@ -10,8 +13,11 @@ import jpabasic.pinnolbe.repository.study.StudyRepository;
 import jpabasic.pinnolbe.service.StudyService;
 import jpabasic.pinnolbe.service.login.UserService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
@@ -23,12 +29,20 @@ public class StudyController {
     private final UserRepository userRepository;
     private final UserService userService;
     private final StudyService studyService;
-    public StudyController(StudyRepository studyRepository,UserRepository userRepository,UserService userService,StudyService studyService) {
+    private final AmazonS3 amazonS3;
+    private final AmazonS3Client amazonS3Client;
+
+    public StudyController(StudyRepository studyRepository, UserRepository userRepository, UserService userService, StudyService studyService, AmazonS3 amazonS3, AmazonS3Client amazonS3Client) {
         this.studyRepository = studyRepository;
         this.userRepository = userRepository;
         this.userService = userService;
         this.studyService = studyService;
+        this.amazonS3 = amazonS3;
+        this.amazonS3Client = amazonS3Client;
     }
+
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucket;
 
 
     //실제 학습
@@ -63,5 +77,24 @@ public class StudyController {
     public ResponseEntity<String> getChapterTitle(@RequestParam String chapterId){
         String title=studyService.getChapterTitle(chapterId);
         return ResponseEntity.ok(title);
+    }
+
+
+
+    @PostMapping(value="/upload-image",consumes="multipart/form-data")
+    @Operation(summary="S3에 학습하기 이미지 업로드")
+    public ResponseEntity<String> uploadFile(@RequestParam("file") MultipartFile file){
+        try{
+            String fileName=file.getOriginalFilename();
+            String fileUrl="https://"+bucket+"/test"+fileName;
+            ObjectMetadata metadata=new ObjectMetadata();
+            metadata.setContentType(file.getContentType());
+            metadata.setContentLength(file.getSize());
+            amazonS3Client.putObject(bucket,fileName,file.getInputStream(),metadata);
+            return ResponseEntity.ok(fileUrl);
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
     }
 }
