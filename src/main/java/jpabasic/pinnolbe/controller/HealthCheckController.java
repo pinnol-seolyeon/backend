3cd38f61ad6c641d958b871f3d11a02525683231
minsikk <cherrykang2003@gmail.com>
2025-06-09 06:26:47 +0900

    refactor : logging level 변경
    - DEBUG => INFO

--- src/main/java/jpabasic/pinnolbe/controller/HealthCheckController.java
+++ src/main/java/jpabasic/pinnolbe/controller/HealthCheckController.java
@@ -6,12 +6,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@Slf4j
+//@Slf4j
 // 로드밸런서 대상 그룹 healthy체크를 하도록 함
 public class HealthCheckController {
 
     @GetMapping("/health-check")
-    public ResponseEntity<String> helathCheck(){
+    public ResponseEntity<String> healthCheck(){
         return ResponseEntity.ok("success");
     }
 }
