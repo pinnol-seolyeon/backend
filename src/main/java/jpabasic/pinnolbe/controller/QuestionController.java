878c80c0e888a2373f7c98a6ee8aeaa4463b7e30
minsikk <cherrykang2003@gmail.com>
2025-10-30 13:30:43 +0900

    ìfeat: 질문하기/질문 저장 api 공통 ìresponse 형식 적용

--- src/main/java/jpabasic/pinnolbe/controller/QuestionController.java
+++ src/main/java/jpabasic/pinnolbe/controller/QuestionController.java
@@ -6,6 +6,7 @@ import jpabasic.pinnolbe.domain.question.QueCollection;
 import jpabasic.pinnolbe.dto.question.QuestionRequest;
 import jpabasic.pinnolbe.dto.question.QuestionResponse;
 import jpabasic.pinnolbe.dto.question.QuestionSummaryDto;
+import jpabasic.pinnolbe.global.ApiResponse;
 import jpabasic.pinnolbe.service.question.QuestionService;
 import jpabasic.pinnolbe.service.study.StudyLogService;
 import jpabasic.pinnolbe.service.login.UserService;
@@ -34,20 +35,20 @@ public QuestionController(QuestionService questionService, UserService userServi
 
     @PostMapping("")
     @Operation(summary="질문하기")
-    public ResponseEntity<QuestionResponse> askQuestion(@RequestBody String question) {
+    public ApiResponse<QuestionResponse> askQuestion(@RequestBody String question) {
         User user=userService.getUserInfo();
         //AI로부터 응답받기
         QuestionResponse response=questionService.askQuestion(question,user);
-        return ResponseEntity.ok(response);
+        return ApiResponse.success("질문 저장 완료",response);
     }
 
 
     @PostMapping("/save-all")
-    @Operation(summary="여태까지 진행한 질문+답변 DB에 저장",
+    @Operation(summary="여태까지 진행한 질문+답변 DB에 저장 및 표현력&참여도 측정",
                 description= """
                         학습하기 3단계 완료 시 해당 api 호출 (session에 저장해두었던 질문/답변/점수 DB에 저장)
                         """)
-    public ResponseEntity<String> saveAllQA(@RequestParam String chapterId){
+    public ApiResponse<String> saveAllQA(@RequestParam String chapterId){
         User user=userService.getUserInfo();
         try {
             //질문한 내용들 DB에 저장
@@ -65,9 +66,9 @@ public QuestionController(QuestionService questionService, UserService userServi
             }
 
         }catch(Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+            return ApiResponse.fail("질문 저장 종 오류가 발생하였습니다.",500);
         }
-        return ResponseEntity.ok("여태까지의 질문&답변이 DB에 무사히 저장되었습니다.");
+        return ApiResponse.success("질문 저장 완료",null);
     }
 
 
