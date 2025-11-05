5ff20e1cad178366ad440b845bcab37597559604
minsikk <131092169+1224kang@users.noreply.github.com>
2025-11-05 22:06:12 +0900

    refactor: 인가증 없이 진입가능한 uri 수정

--- src/main/java/jpabasic/pinnolbe/config/SecurityConfig.java
+++ src/main/java/jpabasic/pinnolbe/config/SecurityConfig.java
@@ -90,7 +90,7 @@ public class SecurityConfig {
 
                 //경로별 인가 작업
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/","/loginForm","/api/oauth/**","/swagger-ui/**","/health-check","/v3/api-docs/**","/swagger-resources/**")
+                        .requestMatchers("/","/loginForm","/api/oauth/**","/swagger-ui/**","/health-check","/v3/api-docs/**","/swagger-resources/**","/api/SSE/**")
                         .permitAll()
                         .anyRequest().authenticated()
 
