26fb4eaadf8d8f3636bc477524f31331fc959b90
minsikk <131092169+1224kang@users.noreply.github.com>
2025-11-17 01:07:19 +0900

    hotfix:userId에 ì단일 인덱스 적용

--- src/main/java/jpabasic/pinnolbe/domain/reward/Reward.java
+++ src/main/java/jpabasic/pinnolbe/domain/reward/Reward.java
@@ -7,6 +7,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.CompoundIndex;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection="reward")
@@ -23,6 +24,7 @@ public class Reward extends BaseEntity {
     @Id
     private String id;
     //유저 기준으로 조회가 매우 많이 일어나므로 인덱스 필수
+    @Indexed
     private String userId;
     private Integer coin;
     private PointCategory category;
