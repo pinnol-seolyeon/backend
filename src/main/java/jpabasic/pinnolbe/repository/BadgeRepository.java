55321fd9b91cb2a21e7728fd194f4f1799b2736d
minsikk <131092169+1224kang@users.noreply.github.com>
2025-11-18 01:59:29 +0900

    hotfix:ê¹같은 배지 중복 저장 되지 않도록 수정

--- src/main/java/jpabasic/pinnolbe/repository/BadgeRepository.java
+++ src/main/java/jpabasic/pinnolbe/repository/BadgeRepository.java
@@ -1,6 +1,7 @@
 package jpabasic.pinnolbe.repository;
 
 import jpabasic.pinnolbe.domain.badge.Badge;
+import jpabasic.pinnolbe.domain.badge.BadgeType;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,4 +11,9 @@ import java.util.List;
 public interface BadgeRepository extends MongoRepository<Badge, String> {
 
     List<Badge> findByUserIdAndChapterId(String userId, String chapterId);
+    boolean existsByUserIdAndChapteridAndBadgeType(
+            String userId,
+            String chapterId,
+            BadgeType badgeType
+    );
 }
