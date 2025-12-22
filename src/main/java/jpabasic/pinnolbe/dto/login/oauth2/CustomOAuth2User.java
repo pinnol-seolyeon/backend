82db44f93062cbb8320ddddff478a5d60b97c3b9
minsikk <131092169+1224kang@users.noreply.github.com>
2025-12-23 07:02:58 +0900

    refactor: 첫 로그인 시 부모님 전화번호 받기

--- src/main/java/jpabasic/pinnolbe/dto/login/oauth2/CustomOAuth2User.java
+++ src/main/java/jpabasic/pinnolbe/dto/login/oauth2/CustomOAuth2User.java
@@ -47,10 +47,7 @@ public class CustomOAuth2User implements OAuth2User {
     }
 
     //첫 로그인인지 확인
-//    public boolean isFirstLogin(){
-//        return userDto.
-//    }
-//     public boolean isFirstLogin(){
-//         return userDto.getPhoneNumber()==null;
-//     }
+    public boolean isFirstLogin(){
+        return userDto.getPhoneNumber()==null;
+    }
 }
